857a897640dd0be7fcb4d5f8cb29560e998666f2
DimaD1989 <d_d1989sv@mail.ru>
2024-01-15 20:10:52 +0300

    FlaskDZ1

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template
+
+# Объект класса Flask создается
+app = Flask(__name__)
+# Во  строке app= создается объект Flask.
+# Для этого конструктору Flask назначается аргумент __name__.
+# Конструктор Flask должен иметь один обязательный аргумент.
+# Им служит название пакета. В большинстве случаев значение __name__ подходит.
+# Название пакета приложения используется фреймворком Flask, чтобы находить статические файлы, шаблоны.
+
+# Создание route (путей)
+@app.route('/')
+@app.route('/main/')
+# Маршрут (или путь) используется во фреймворке Flask для привязки URL к функции представления.
+# Эта функция отвечает на запрос.
+# Во Flask декоратор route используется, чтобы связать URL адрес с функций.
+def main_page():
+    context = {'title': 'Интернет-магазин: Главная'}
+    return render_template('main.html', **context)
+
+@app.route('/clothes/')
+def clothes_page():
+    context = {'title': 'Интернет-магазин: Одежда'}
+    return render_template('clothes.html', **context)
+
+@app.route('/footwear/')
+def footwear_page():
+    context = {'title': 'Интернет-магазин: Обувь'}
+    return render_template('footwear.html', **context)
+
+if __name__=='__main__':
+    app.run()
+    # Условие __name__ == "__main__" гарантирует,
+    # что метод run() будет вызван только в том случае,
+    # если main.py будет запущен,
+    # как основная программа.
+    # Если попытаться использовать метод run() при импорте main.py в другой модуль Python,
+    # он не вызовется.
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from main import app
+# Режим отладки (Debug)
+# Чтобы включить режим, нужно передать аргумент debug=True методу run()
+if __name__ == '__main__':
+    app.run(debug=True)
